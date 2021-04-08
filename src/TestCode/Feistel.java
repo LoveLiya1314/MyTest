331144f06ebb5cbf4c0059ca67e3e4df2939d9a4
1841302343@qq.com <1841302343@qq.com>
2021-04-08 23:25:23 +0800

    默认更改列表

--- src/TestCode/Feistel.java
+++ src/TestCode/Feistel.java
@@ -1,6 +1,6 @@
 /******************************************************************************
  * VIDI VINI VIKI                                                             *
- * Copyright ©  21:58 2021 -4 -8  Lambert All rights reserved.                *
+ * Copyright ©  23:25 2021 -4 -8  Lambert All rights reserved.                *
  *    Licensed under the Apache License, Version 2.0 (the "License");         *
  *    you may not use this file except in compliance with the License.        *
  *    You may obtain a copy of the License at                                 *
@@ -16,7 +16,6 @@
  ******************************************************************************/
 
 package TestCode;
-
 public class Feistel {
 
 
--- src/TestCode/test9.java
+++ src/TestCode/test9.java
@@ -1,3 +1,20 @@
+/******************************************************************************
+ * VIDI VINI VIKI                                                             *
+ * Copyright ©  23:25 2021 -4 -8  Lambert All rights reserved.                *
+ *    Licensed under the Apache License, Version 2.0 (the "License");         *
+ *    you may not use this file except in compliance with the License.        *
+ *    You may obtain a copy of the License at                                 *
+ *                                                                            *
+ *      http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                            *
+ *    Unless required by applicable law or agreed to in writing, software     *
+ *    distributed under the License is distributed on an "AS IS" BASIS,       *
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+ *    See the License for the specific language governing permissions and     *
+ *    limitations under the License.                                          *
+ * Love Liya Forever!                                                         *
+ ******************************************************************************/
+
 package TestCode;
 
 import java.util.Scanner;
@@ -7,6 +24,7 @@ public class test9 {
         System.out.println("GoodBay,World!");
         Scanner in = new Scanner(System.in);
         int i=0;
-        i=in.nextInt();
+        i = in.nextInt();
+        System.out.println(i);
     }
 }
