4754ac106a8826f38cc2f7093b314cf441238556
Lambert <Loveliya1314@foxmail.com>
2021-04-21 17:03:51 +0800

    change error

--- src/ApplicationCode/Feistel.java
+++ src/ApplicationCode/Feistel.java
@@ -1,6 +1,6 @@
 /******************************************************************************
  * VIDI VINI VIKI                                                             *
- * Copyright ©  23:25 2021 -4 -8  Lambert All rights reserved.                *
+ * Copyright ©  17:3 2021 -4 -21  Lambert All rights reserved.                *
  *    Licensed under the Apache License, Version 2.0 (the "License");         *
  *    you may not use this file except in compliance with the License.        *
  *    You may obtain a copy of the License at                                 *
@@ -15,7 +15,7 @@
  * Love Liya Forever!                                                         *
  ******************************************************************************/
 
-package AplicationCode;
+package ApplicationCode;
 public class Feistel {
 
 
